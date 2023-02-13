f57c3be902e39217a69692d0b3fa0461e766498a
acleal <andrew@emteq.net>
2023-02-13 17:32:43 +0000

    new framework.zip & checksum
    new framework.zip & checksum

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
 //			path: "/Users/andycleal/Documents/GitHub/expEng/Sources/expEng.xcframework.zip"		// local
 //			url: "https://github.com/emteqlabs/\(moduleName)/releases/download/\(version)/\(moduleName).xcframework.zip",
 //			url: "https://github.com/emteqlabs/\(moduleName)/blob/main/Sources/\(moduleName).xcframework.zip",
-			url: "https://github.com/emteqlabs/eeFrame/blob/main/eeFrame.xcframework.zip",			
-			checksum: "f8a0fb30f2baa39b9910ddb7fb094e35d7c9149322470d1c5e3b95b4115e13e0"
+			url: "https://github.com/emteqlabs/eeFrame/blob/main/eeFrame.xcframework.zip",
+			checksum: "bfaf6710460986083f91523a57744497e197272629187d0c1422b9df05941f3f"
 		)
 	]
 )
