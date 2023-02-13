ee6a9501d1a3a7510895150e5feabefae9fe7bc4
acleal <andrew@emteq.net>
2023-02-13 15:05:48 +0000

    edited package.swift
    edited package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
 //			path: "/Users/andycleal/Documents/GitHub/expEng/Sources/expEng.xcframework.zip"		// local
 //			url: "https://github.com/emteqlabs/\(moduleName)/releases/download/\(version)/\(moduleName).xcframework.zip",
 //			url: "https://github.com/emteqlabs/\(moduleName)/blob/main/Sources/\(moduleName).xcframework.zip",
-			url: "https://github.com/emteqlabs/expEng/blob/main/Sources/eeFrame/eeFrame.xcframework.zip",
+			url: "https://github.com/emteqlabs/eeFrame/blob/main/eeFrame.xcframework.zip",			
 			checksum: "f8a0fb30f2baa39b9910ddb7fb094e35d7c9149322470d1c5e3b95b4115e13e0"
 		)
 	]
